2985e93e5ca6027ee23fed8a840a315beceec696
Krystian Gwizdala <kgwizdal@cisco.com>
2019-04-27 07:17:26 +0200

    move scripts / styles to separate file

---
+++ spark/scripts/mailing.js
@@ -0,0 +1,14 @@
+if (typeof acymailingModule == 'undefined'){
+var acymailingModule = [];
+}
+acymailingModule['emailRegex'] = /^[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*\@([a-z0-9-]+\.)+[a-z0-9]{2,10}$/i;
+acymailingModule['NAMECAPTION'] = 'Enter your email';
+acymailingModule['NAME_MISSING'] = 'Please enter your name';
+acymailingModule['EMAILCAPTION'] = 'Enter your email';
+acymailingModule['VALID_EMAIL'] = 'Please enter a valid e-mail address';
+acymailingModule['ACCEPT_TERMS'] = 'Please check the Terms and Conditions';
+acymailingModule['CAPTCHA_MISSING'] = 'The captcha is invalid, please try again';
+acymailingModule['NO_LIST_SELECTED'] = 'Please select the lists you want to subscribe to';
+ var sp_preloader = '0';
+ var sp_gotop = '1';
+ var sp_offanimation = 'default';
